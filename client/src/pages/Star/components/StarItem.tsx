6baa9bad7ea48c50b6359333c73a0ce60429ffbe
rkskekzzz <wkdlfflxh@naver.com>
2022-08-19 23:54:45 +0900

    ✨ 아몰랑

--- client/src/pages/Star/components/StarItem.tsx
+++ client/src/pages/Star/components/StarItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Locker } from 'src/types/Locker';
+import { useNavigate } from 'react-router';
 import { ReactComponent as StarProfileIcon } from 'src/assets/star/프로필 아이콘.svg';
 import StarItemContainer from './StarItem.style';
 
@@ -8,10 +9,14 @@ interface StarItemProps {
 }
 
 const StarItem = ({ lockers }: StarItemProps) => {
+  const navigate = useNavigate();
+
+  const handleItemTabbed = (id: string) => navigate(`/locker/${id}`);
+
   return (
     <StarItemContainer>
       {lockers.map((locker) => (
-        <span key={locker.id}>
+        <span key={locker.id} onClick={() => handleItemTabbed(locker.id)}>
           <StarProfileIcon />
           <h2>{locker.location}</h2>
         </span>
